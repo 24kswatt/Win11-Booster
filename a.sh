2bbcc6f8c06a8530113448bb2aceb525ce74bc42
24k.swatt <pastaragu254@gmail.com>
2026-05-03 17:38:32 +0200

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
 sudo pacman -Syu --needed \
   hyprland xorg-xwayland \
-  waybar tofi kitty \
+  waybar fuzzel kitty \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
   qt5-wayland qt6-wayland \
   pavucontrol pamixer \
@@ -13,5 +10,5 @@ sudo pacman -Syu --needed \
   thunar thunar-archive-plugin file-roller tumbler gvfs udiskie \
   polkit-kde-agent \
   noto-fonts noto-fonts-emoji ttf-jetbrains-mono-nerd \
-  papirus-icon-theme bibata-cursor-theme nwg-look \
+  papirus-icon-theme adwaita-cursors nwg-look \
   lazygit tmux fzf ripgrep fd eza bat zoxide starship btop jq
